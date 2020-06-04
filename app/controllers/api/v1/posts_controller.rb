ac96ac2858bee473dc624fde66f058078ac0b641
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-04 12:11:09 +0100

    refactor posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,21 +1,25 @@
 class Api::V1::PostsController < ActionController::Base
+    before_action :set_post, only: [ :show, :update, :destroy]
+
     def index 
         @posts = Post.all
         render json: @posts
     end
 
     def show 
-        @post = Post.find(params[:id])
         render json: @post
     end
 
     def create 
-        @post = Post.create(post_params)
-        render json: @post
+        if @post = Post.create(post_params)
+          render json: @post
+        else
+            render_error
+        end
     end
 
     def update 
-        @post = Post.find(params[:id])
+        # @post = Post.find(params[:id])
         if @post.update(post_params)
             render json: @post
         else
@@ -24,7 +28,7 @@ class Api::V1::PostsController < ActionController::Base
     end
 
     def destroy 
-        @post = Post.find(params[:id])
+        # @post = Post.find(params[:id])
         @post.destroy 
         head :no_content
     end
@@ -33,6 +37,15 @@ class Api::V1::PostsController < ActionController::Base
     private
         def post_params
             params.require(:post).permit(:title, :categories, :content)
+        end
+
+        def set_post 
+            @post = Post.find(params[:id])
+        end   
+
+        def render_error
+            render json: { errors: post.errors.full_messages },
+            status: :unprocessable_entity
         end
 
 
