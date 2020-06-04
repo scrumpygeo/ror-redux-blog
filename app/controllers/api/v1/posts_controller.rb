a3e7d1bd374a4a5b98cbfa9b4da71c4e25784985
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-04 11:19:31 +0100

    add api routes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::PostsController < ActionController::Base
-    
+    def index 
+        @posts = Post.all
+        render json: @posts
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   
   get "posts/:id", to: 'pages#home' 
   get "posts/new", to: 'pages#home'
+
+   # API routing   
+   namespace :api, defaults: { format: :json } do     
+    namespace :v1 do       
+      resources :posts, only: [ :index, :show, :create, :update, :destroy ]     
+    end 
+  end
 end
