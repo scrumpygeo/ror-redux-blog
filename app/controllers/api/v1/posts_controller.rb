f708d4b09fccfdb728453c99f691812732329b11
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-04 12:00:06 +0100

    show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,6 +4,11 @@ class Api::V1::PostsController < ActionController::Base
         render json: @posts
     end
 
+    def show 
+        @post = Post.find(params[:id])
+        render json: @post
+    end
+
     def create 
         @post = Post.create(post_params)
         render json: @post
