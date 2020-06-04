b6faf20c5ea7f0e65626f4483f80c5b0a25696d8
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-06-04 11:51:05 +0100

    create action

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,4 +3,16 @@ class Api::V1::PostsController < ActionController::Base
         @posts = Post.all
         render json: @posts
     end
+
+    def create 
+        @post = Post.create(post_params)
+        render json: @post
+    end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :categories, :content)
+        end
+
+
 end
